ebaf388ec179300cbfdd5443039253bfbeb53774
AdrianoCalo-hub <ramarolahycarlo11@gmail.com>
2025-06-15 11:48:16 +0200

    projet Boost C# crud

---
+++ visual_Sudio2012/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace visual_Sudio2012
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNom = new System.Windows.Forms.Label();
+            this.labelId = new System.Windows.Forms.Label();
+            this.labelPrenom = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.textBoxPrenom = new System.Windows.Forms.TextBox();
+            this.textBoxNom = new System.Windows.Forms.TextBox();
+            this.buttonAjouter = new System.Windows.Forms.Button();
+            this.Supprimer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNom
+            // 
+            this.labelNom.AutoSize = true;
+            this.labelNom.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNom.Location = new System.Drawing.Point(46, 90);
+            this.labelNom.Name = "labelNom";
+            this.labelNom.Size = new System.Drawing.Size(50, 19);
+            this.labelNom.TabIndex = 0;
+            this.labelNom.Text = "Nom :";
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelId.Location = new System.Drawing.Point(36, 45);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(81, 19);
+            this.labelId.TabIndex = 1;
+            this.labelId.Text = "Identifient:";
+            // 
+            // labelPrenom
+            // 
+            this.labelPrenom.AutoSize = true;
+            this.labelPrenom.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrenom.Location = new System.Drawing.Point(46, 152);
+            this.labelPrenom.Name = "labelPrenom";
+            this.labelPrenom.Size = new System.Drawing.Size(66, 19);
+            this.labelPrenom.TabIndex = 2;
+            this.labelPrenom.Text = "Prenom:";
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAge.Location = new System.Drawing.Point(46, 210);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(40, 19);
+            this.labelAge.TabIndex = 3;
+            this.labelAge.Text = "Age:";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(146, 42);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.ReadOnly = true;
+            this.textBoxId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxId.TabIndex = 4;
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(146, 203);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(140, 20);
+            this.textBoxAge.TabIndex = 5;
+            // 
+            // textBoxPrenom
+            // 
+            this.textBoxPrenom.Location = new System.Drawing.Point(146, 149);
+            this.textBoxPrenom.Name = "textBoxPrenom";
+            this.textBoxPrenom.Size = new System.Drawing.Size(140, 20);
+            this.textBoxPrenom.TabIndex = 6;
+            // 
+            // textBoxNom
+            // 
+            this.textBoxNom.Location = new System.Drawing.Point(146, 91);
+            this.textBoxNom.Name = "textBoxNom";
+            this.textBoxNom.Size = new System.Drawing.Size(140, 20);
+            this.textBoxNom.TabIndex = 7;
+            // 
+            // buttonAjouter
+            // 
+            this.buttonAjouter.BackColor = System.Drawing.Color.DimGray;
+            this.buttonAjouter.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonAjouter.Location = new System.Drawing.Point(68, 325);
+            this.buttonAjouter.Name = "buttonAjouter";
+            this.buttonAjouter.Size = new System.Drawing.Size(103, 29);
+            this.buttonAjouter.TabIndex = 8;
+            this.buttonAjouter.Text = "Ajouter";
+            this.buttonAjouter.UseVisualStyleBackColor = false;
+            this.buttonAjouter.Click += new System.EventHandler(this.buttonAjouter_Click);
+            // 
+            // Supprimer
+            // 
+            this.Supprimer.BackColor = System.Drawing.Color.DimGray;
+            this.Supprimer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Supprimer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Supprimer.Location = new System.Drawing.Point(204, 381);
+            this.Supprimer.Name = "Supprimer";
+            this.Supprimer.Size = new System.Drawing.Size(96, 29);
+            this.Supprimer.TabIndex = 9;
+            this.Supprimer.Text = "Supprimer";
+            this.Supprimer.UseVisualStyleBackColor = false;
+            this.Supprimer.Click += new System.EventHandler(this.Supprimer_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.BackColor = System.Drawing.Color.DimGray;
+            this.btnModifier.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnModifier.Location = new System.Drawing.Point(68, 381);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(103, 29);
+            this.btnModifier.TabIndex = 10;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = false;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.BackColor = System.Drawing.Color.DimGray;
+            this.btnAnnuler.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAnnuler.Location = new System.Drawing.Point(204, 325);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(96, 29);
+            this.btnAnnuler.TabIndex = 13;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.DimGray;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(426, 21);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(502, 303);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 422);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.Supprimer);
+            this.Controls.Add(this.buttonAjouter);
+            this.Controls.Add(this.textBoxNom);
+            this.Controls.Add(this.textBoxPrenom);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelPrenom);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.labelNom);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.ActiveBorder;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNom;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label labelPrenom;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.TextBox textBoxPrenom;
+        private System.Windows.Forms.TextBox textBoxNom;
+        private System.Windows.Forms.Button buttonAjouter;
+        private System.Windows.Forms.Button Supprimer;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ visual_Sudio2012/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace visual_Sudio2012
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnx;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public Form1()
+        {
+            cnx = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=N:\Programme_Visual_Studio\AprojetC#\visual_Sudio2012\visual_Sudio2012\Etudiant.mdf;Integrated Security=True");
+            InitializeComponent();
+            cnx.Open();
+            cmd = new SqlCommand("Select*from etudiant", cnx);
+            dr = cmd.ExecuteReader();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Add("Id", "Id");
+            dataGridView1.Columns.Add("Nom", "Nom");
+            dataGridView1.Columns.Add("Prenom", "Prenom");
+            dataGridView1.Columns.Add("Age", "Age");
+            int i = 0;
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = dr["Id"];
+                dataGridView1.Rows[i].Cells[1].Value = dr["Nom"];
+                dataGridView1.Rows[i].Cells[2].Value = dr["Prenom"];
+                dataGridView1.Rows[i].Cells[3].Value = dr["Age"];
+                i++;
+            }
+            dr.Dispose();
+            cnx.Close(); 
+            actualiser();
+              
+        }
+
+        private void buttonAjouter_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+
+            string sql = "INSERT INTO Etudiant (Nom, Prenom, Age) VALUES (@nom, @prenom, @age)";
+            SqlCommand cmd = new SqlCommand(sql, cnx);
+            cmd.Parameters.AddWithValue("@nom", textBoxNom.Text);
+            cmd.Parameters.AddWithValue("@prenom", textBoxPrenom.Text);
+            cmd.Parameters.AddWithValue("@age", textBoxAge.Text);
+            cmd.ExecuteNonQuery();
+
+            cnx.Close();
+            actualiser(); 
+            videLesChamps(); 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                textBoxId.Text = row.Cells["Id"].Value.ToString();
+                textBoxNom.Text = row.Cells["Nom"].Value.ToString();
+                textBoxPrenom.Text = row.Cells["Prenom"].Value.ToString();
+                textBoxAge.Text = row.Cells["Age"].Value.ToString();
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Supprimer_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+            SqlCommand cmd = new SqlCommand("Delete from etudiant where Id = @id", cnx);
+            cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Suppression réussi!");
+            cnx.Close();
+            actualiser();
+            videLesChamps();
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            cnx.Open();
+            SqlCommand cmd = new SqlCommand("Update etudiant set Nom = @nom, Prenom = @prenom, Age = @age where Id = @id", cnx);
+            cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+            cmd.Parameters.AddWithValue("@nom", textBoxNom.Text);
+            cmd.Parameters.AddWithValue("@prenom", textBoxPrenom.Text);
+            cmd.Parameters.AddWithValue("@age", textBoxAge.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Modification réussi!");
+            cnx.Close();
+            actualiser();
+            videLesChamps();
+        }
+        private void videLesChamps(){
+            textBoxId.Text = "";
+            textBoxNom.Text = "";
+            textBoxPrenom.Text = "";
+            textBoxAge.Text = "";
+        }
+        private void actualiser()
+        {
+            cnx.Open();
+            cmd = new SqlCommand("Select*from etudiant", cnx);
+            dr = cmd.ExecuteReader();
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Add("Id", "Id");
+            dataGridView1.Columns.Add("Nom", "Nom");
+            dataGridView1.Columns.Add("Prenom", "Prenom");
+            dataGridView1.Columns.Add("Age", "Age");
+            int i = 0;
+            while (dr.Read())
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = dr["Id"];
+                dataGridView1.Rows[i].Cells[1].Value = dr["Nom"];
+                dataGridView1.Rows[i].Cells[2].Value = dr["Prenom"];
+                dataGridView1.Rows[i].Cells[3].Value = dr["Age"];
+                i++;
+            }
+            dr.Dispose();
+            cnx.Close();
+        }
+
+    }
+    
+}
+
